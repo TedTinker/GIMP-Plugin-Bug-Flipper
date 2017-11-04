722919f8aafca41ac2a141387482ddf7407f70eb
Ted <TedJTinker@gmail.com>
2017-11-04 12:49:41 -0700

    Add files via upload

--- GenericBugFlipper.py
+++ GenericBugFlipper.py
@@ -7,7 +7,7 @@ This utylizes a WhiteBalanceStretch freeware plugin from Diego Nassetti.
 
 Once installed, Bug-Flipper appears in the GIMP menus under Filters.
 Click it, and a dialog-box prompts the user for two folders and some options.
-The program will open certain images in the first folder. It can rotate the image 180 degrees,
+The program will open certain images in the first folder. It can rotate the image in increments of 90 degrees,
 color correct it, display it for more editing, and open another dialog prompt requesting a name.
 If possible, rename the image using the scanner gun on the QR code displayed in the image.
 If the photo is so blurry it's difficult to read the labels, mark the checkbox to prepend "bad_pic_" to the filename.
@@ -34,30 +34,30 @@ Then the image is saved with that filename in the second folder with the desired
 '''
 
 
+#####
+
 
 #!/usr/bin/env python
 
 from gimpfu import * 	# For interacting with the GIMP
 import os		# For pulling files from folders
 
-import gtk		# The gtk code is adapted from Ben Duffield's Ardonis Wordpress. Thanks, Ben!
-			# It's used for generating dialog boxes to prompt users for new filenames.
-			# It comes with a checkbox marked "Flag bad image?" If checked, "bad_pic_" appended to filename start 
+import gtk		# Used for generating dialog boxes to prompt users for new filenames.
+			# It comes with a checkbox marked "Flag bad image?" If checked, "bad_pic_" prepended to filename 
 
 ##### 
 
-def responseToDialog(entry, dialog, response):
+def responseToDialog(entry, dialog, response):		# This makes renaming with the scanner gun quite fast, as the gun hits enter
     dialog.response(response)
-def getText(file):
-    #base this on a message dialog
+def getText(file):					# Brings up dialog box for renaming
     dialog = gtk.MessageDialog(
         None,
         gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
         gtk.MESSAGE_QUESTION,
         gtk.BUTTONS_OK,None)
-    dialog.set_markup('Edit the photo now. Then enter new file name. Use the scanner-gun on the QR if possible. Leave the space blank to use old file name.')
-    checkBlurry = gtk.CheckButton() 	# Add checkmark box
-    entry = gtk.Entry()			# Add text-entry box				
+    dialog.set_markup('Edit the photo now. Then enter new file name. Use the scanner gun on the QR if possible. If no text is entered, the old filename will be used.')
+    checkBlurry = gtk.CheckButton() 					# Add checkmark box
+    entry = gtk.Entry()							# Add text-entry box				
     entry.connect("activate", responseToDialog, dialog, gtk.RESPONSE_OK)
     vbox = gtk.VBox(TRUE)						# Vertical box
     hbox1 = gtk.HBox(TRUE)						# First horizontal box has text-entry
@@ -78,19 +78,19 @@ def getText(file):
     if(text == ""):			# If the dialog is returned empty, use old file name
 	text = file
 
-    if(checkBlurry.get_active()):	# If flagged, start filename with "bad_pic_"
+    if(checkBlurry.get_active()):	# If checkbox flagged, start filename with "bad_pic_"
 	dialog.destroy()
 	return "bad_pic_" + text
 
-    dialog.destroy() 			# Otherwise, return filename
+    dialog.destroy() 			# Return filename
     return text
 
 #####
 
 
-def BugFlipper(OldDir,NewDir,renameMe,rotateMe,correctMe,deleteOld,imageQuality):
+def BugFlipper(OldDir,NewDir,begins,ends,renameMe,rotateMe,correctMe,deleteOld,imageQuality):
 	for file in os.listdir(OldDir):						# Checks every file in first folder
-		if ((file.endswith(".jpg") or file.endswith(".JPG")) and file.startswith("DSC_")): 	# Selects JPEGS beginning DSC_
+		if (file.endswith(ends) and file.startswith(begins)): 		# Selects files with prefix and suffix
 			PrepareImage(file,OldDir,NewDir,renameMe,rotateMe,correctMe,imageQuality)	
 										# Method for image processing
 		if (deleteOld):
@@ -102,8 +102,8 @@ def BugFlipper(OldDir,NewDir,renameMe,rotateMe,correctMe,deleteOld,imageQuality)
 def PrepareImage(file,OldDir,NewDir,renameMe,rotateMe,correctMe,imageQuality):			# Processes photos
 	image = pdb.gimp_file_load(OldDir+"/"+file,OldDir+"/"+file)
 	drawable = pdb.gimp_image_get_active_layer(image)			
-	if(rotateMe):
-		pdb.gimp_drawable_transform_rotate_simple(drawable,ROTATE_180, 1,0,0,1)		# Rotate 180 degrees, if selected
+	if(rotateMe!=3):
+		pdb.gimp_image_rotate(image,rotateMe)						# Rotate desired amount
 	if(correctMe):
 		drawable = pdb.python_fu_WhiteBalanceStretch(image,drawable)			# White/Color balance by Diego Nassetti, if selected
 	newFileName = file
@@ -112,30 +112,39 @@ def PrepareImage(file,OldDir,NewDir,renameMe,rotateMe,correctMe,imageQuality):		
 		newFileName = getText(file) 							# Asks user for new filename
 	pdb.file_jpeg_save(image, drawable,NewDir+"/"+newFileName,NewDir+"/"+newFileName,
 		imageQuality, 0,0,0,"newFileName",0,1,0,0)					# Save in second folder with new name and quality
-	if(renameMe or cropMe):	
+	if(renameMe):	
 		pdb.gimp_display_delete(display)						# Remove display
 
 #####
 
 register(
-   	"Bug-Flipper",						# Name
+   	"Bug-Flipper-Generic",					# Name
    	"Made by Ted Tinker for Cheadle Center's zoologists",	# Blurb
    	"Made by Ted Tinker for Cheadle Center's zoologists for human-assisted-image-processing of standardized insect photos.",	# Help
 	"Ted Tinker",						# Author
 	"Ted Tinker, freeware",					# Copywrite
     	"2017",							# Date
-    	"Bug-Flipper",						# Display Name
+    	"Bug-Flipper-Generic",					# Display Name
     	"",      						# No picture required
-    [
-	(PF_DIRNAME, "OldDir", "Folder with photos:","C:\\"),	# Method Parameters
-	(PF_DIRNAME, "NewDir", "Folder to save photos:", "C:\\"),
-	(PF_BOOL, "renameMe", "Rename/Edit photos?", 1),
-	(PF_BOOL, "rotateMe", "Rotate photos 180 degrees?", 1),
-	(PF_BOOL, "correctMe", "Run white-balance/color-correction?", 1),
-	(PF_BOOL, "deleteOld", "Delete old photos after processing?", 0),
-	(PF_SLIDER, "imageQuality", "Saved Image Quality:", .3,(.01,1,.01))
+    [								# Method Parameters
+	(PF_DIRNAME, "OldDir", "Folder with photos:","C:\\"),		# First Folder; replace C:\\ with default
+	(PF_DIRNAME, "NewDir", "Folder to save photos:", "C:\\"),	# Second Folder; replace C:\\ with default
+	(PF_STRING, "begins", "Filenames begin with...", "DSC_"),	# Prefix. Default to opening raw camera photos, which start this way
+	(PF_STRING, "ends", "Filenames end with...", ".jpg"),		# Suffix. Default to opening raw camera photos, which are jpegs
+	(PF_BOOL, "renameMe", "Rename/Edit photos?", 1),		# Should the image be displayed for editing/renaming?
+	(PF_RADIO, "rotateMe", "Rotate photos?", 3, (			# Choose rotation, default none
+		("No", 3),
+		("Clockwise 90 degrees",0),
+		("Rotate 180 degrees",1),
+		("Counterclockwise 90 degrees",2))),
+	(PF_BOOL, "correctMe", "Run white-balance/color-correction?", 1),	# Color correct? Default yes
+	(PF_BOOL, "deleteOld", "Delete old photos after processing?", 0),	# Delete old photos? Default no
+	(PF_SLIDER, "imageQuality", "Saved Image Quality:", .3,(.01,1,.01))	# Choose compression level. 1 is no compression, .01 is most compressed.
+										# Default of .3 is almost as clear as uncompressed image, but saves memory.
     ],
     [],								# Method Return
     BugFlipper, menu="<Toolbox>/Filters")
 
 main()
+
+	
